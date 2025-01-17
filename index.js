3cc91a1cec698efb0793dd4a04b643acfd8021d5
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2025-01-17 12:39:35 +0530

    Updating the code

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const http = require("http");
+const WebSocket = require("ws");
 const express = require("express");
-const path = require("path");
-const { Server } = require("socket.io");
 const app = express();
-
-const PORT = 9000;
-const server = http.createServer(app);
-const io = new Server(server);
+const path = require("path");
+const PORT = 7001;
 
 
-//* Socket.io 
-io.on("connection", (socket) => {
-    //? console.log("A new user has connected", socket.id);
-    socket.on("clientMessage", (clientMsg) => {
-        console.log(`A new user message : ${clientMsg}`);
 
-        io.emit("message", clientMsg);
-    });
-});
-
-
-let staticPath = path.join(__dirname, "/public");
-app.use(express.static(staticPath));
-
-
+//* Displaying the HTML file 
 app.get("/", (req, res) => {
-    let htmlFilePath = path.join(__dirname, "/public/index.html");
-    return res.sendFile(htmlFilePath);
+    let filePath = path.join(__dirname, "/public/index.html");
+    res.sendFile(filePath);
 });
 
 
-server.listen(PORT, () => {
-    console.log(`Server started at Port ${PORT}`);
+
+//* Creating a simple http server  
+const httpServer = app.listen(PORT, () => {
+    console.log("Listening on PORT number 7001");
+});
+
+
+
+//* Creating a web socket server 
+const wss = new WebSocket.Server({
+    server : httpServer
+});
+
+
+
+//* Now, we are going to establish a connection using web socket server and pass a 'ws' object in it 
+wss.on("connection", (ws) => {
+
+    //* What you want me to do, when i get a open request from the web socket connection 
+    ws.on("open", () => {
+        console.log(`A new connection is opened!!!!`);
+    });
+    
+    //* What you want me to do when i get a message from the client 
+    ws.on("message", (msg) => {
+
+        //* I want to broadcast that msg to all the connected clients 
+        wss.clients.forEach((client) => {
+            if(client != ws)
+            client.send(msg.toString());
+        });
+    });
+
+    //* What you want me to do, when i receive close request 
+    ws.on("close", () => {
+        console.log("A client is lost");
+    });
+
+
+    console.log("A new client is connected");
+
 });
